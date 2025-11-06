8b9b44f13928f9d73b3264045b93d2ca4a6d8bf3
HiroeIWMT <hiroeiwamoto1@gmail.com>
2025-11-06 13:18:22 +0100

    TP module6 JWK

--- src/main/java/fr/eni/cave/bo/Utilisateur.java
+++ src/main/java/fr/eni/cave/bo/Utilisateur.java
@@ -32,4 +32,6 @@ public class Utilisateur {
     @Column(name="FIRST_NAME", nullable = false, length = 150)
     private String prenom;
 
+    @Column(length = 15)
+    private String authority;
 }
---
+++ src/main/java/fr/eni/cave/security.java
@@ -0,0 +1,4 @@
+package fr.eni.cave;
+
+public class security {
+}
